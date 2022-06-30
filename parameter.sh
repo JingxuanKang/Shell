00e9d68b85d8d1bb66f85d2e3d2ebc251831023a
JingxuanKang <jxkang01@gmail.com>
2022-06-30 20:13:24 +0100

    parameter

--- parameter.sh
+++ parameter.sh
@@ -3,3 +3,9 @@ echo '=========$n========='
 echo script name:$0
 echo 1st parameter:$1
 echo 2st parameter:$2
+echo '=========$n========='
+echo parameter numbers:$#
+echo parameters: $*
+echo parameters: $@
+
+
